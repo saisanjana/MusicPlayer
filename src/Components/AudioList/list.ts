d6407cc7686eaaa63dd39bac1fc65ac60017ce4b
Sanjana <sanjana@NHD050542.local>
2024-03-24 20:02:01 +0530

    changes

--- src/Components/AudioList/list.ts
+++ src/Components/AudioList/list.ts
@@ -1,37 +1,37 @@
 export const SongsList:any  = [
     {
         name:"Heeriye",
-        src:"/MusicPlayer/_Heeriye_320(PagalWorld.com.cm).mp3",
+        src:"/MusicPlayer/src/assets/_Heeriye_320(PagalWorld.com.cm).mp3",
         index:0
     },
     {
         name:"Sample",
-        src:"/MusicPlayer/sample-3s.mp3",
+        src:"/MusicPlayer/src/assets/sample-3s.mp3",
         index:1
     },
     {
         name:"Sample2",
-        src:"/MusicPlayer/sample-3s.mp3",
+        src:"/MusicPlayer/src/assets/sample-3s.mp3",
         index:2
     },
     {
         name:"Ram Siya Ram",
-        src:"/MusicPlayer/_Ram Siya Ram_320(PagalWorld.com.cm).mp3",
+        src:"/MusicPlayer/src/assets/_Ram Siya Ram_320(PagalWorld.com.cm).mp3",
         index:3
     },
     {
         name:"O Mahi",
-        src:"/MusicPlayer/O Mahi O Mahi_320(PagalWorld.com.cm).mp3",
+        src:"/MusicPlayer/src/assets/O Mahi O Mahi_320(PagalWorld.com.cm).mp3",
         index:4
     },
     {
         name:"Pehle Bhi Main",
-        src:"/MusicPlayer/Pehle Bhi Main_320(PagalWorld.com.cm).mp3",
+        src:"/MusicPlayer/src/assets/Pehle Bhi Main_320(PagalWorld.com.cm).mp3",
         index:5
     },
     {
         name: "Ve Haniya",
-        src:"/MusicPlayer/Ve Haniya_320(PagalWorld.com.cm).mp3",
+        src:"/src/assets/Ve Haniya_320(PagalWorld.com.cm).mp3",
         index:6
     },
     
